e746d3f802f127167a6bb7c2a8fe1cb9bf6f56e6
emmao <emf002@edu.zealand.dk>
2024-05-23 15:28:57 +0200

    Titlen er ændret til Navbar og gjort klar til man kan indsætte den til forsiden

---
+++ navbar.php
@@ -0,0 +1,18 @@
+
+
+
+<body class="">
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="bg-sekundærfarve">
+                <div class="">Logo</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
