37df8916ba0fd409aae02a3738af5eeeae6dadf2
emmao <emf002@edu.zealand.dk>
2024-05-23 21:44:06 +0200

    Ændret i modalvinduet

--- navbar.php
+++ navbar.php
@@ -17,7 +17,7 @@
                     </div>
 
                     <div class="d-flex align-items-center">
-                        <a href="#" class="">
+                        <a href="#" class="nav-link" data-bs-toggle="modal" data-bs-target="#sprogModal">
                             <img src="#" alt="" class="img-fluid"> Sprog
                         </a>
                         <a href="#" class="ms-4">
@@ -30,8 +30,42 @@
             <div class="col-1"></div>
         </div>
     </div>
-</nav
+</nav>
 
+
+<div class="modal fade" id="sprogModal" tabindex="-1" aria-labelledby="sprogModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content border-outlinefarve ">
+
+            <div class="modal-header">
+                <h5 class="modal-title" id="sprogModalLabel"></h5>
+                <button type="button" class="btn-close btn-close-primærfarve lukkeknap" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+                <p class="fs-2 text-primærtekstfarve brødtekst">Hvilket sprog foretrækker du?</p>
+                <div class="form-check">
+                    <input class="form-check-input form-check-input" type="radio" name="sprog" id="dansk" value="dansk" checked>
+                    <label class="form-check-label ms-2" for="dansk">Dansk</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="sprog" id="english" value="english">
+                    <label class="form-check-label ms-2" for="english">English</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="sprog" id="deutsch" value="deutsch">
+                    <label class="form-check-label ms-2" for="deutsch">Deutsch</label>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary me-3 btn-lg rounded-pill btn-sekundærknap text-primærtekstfarve border-outlinefarve" data-bs-dismiss="modal" style="width: 150px;">Annuller</button>
+                <button type="button" class="btn btn-primary btn-lg rounded-pill btn-primærknap " data-bs-dismiss="modal" style="width: 150px;">Gem</button>
+            </div>
+
+        </div>
+    </div>
+</div>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
