558213a3561f7321b01fd3a49a39ea33d8d5acb5
emmao <emf002@edu.zealand.dk>
2024-05-23 19:07:14 +0200

    Navbar er oprettet mere fyldestgørende - Mangler ikoner og logo

--- navbar.php
+++ navbar.php
@@ -1,17 +1,36 @@
 
 
+<body>
 
-<body class="">
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            <div class="bg-sekundærfarve">
-                <div class="">Logo</div>
+<nav class="navbar bg-sekundærfarve">
+    <div class="container-fluid">
+        <div class="row w-100">
+            <div class="col-1"></div>
+
+            <div class="col-10">
+                <div class="d-flex justify-content-between display-3 brødtekst">
+                    <div class="">
+                        <a href="#" class="d-flex">
+                            <img src="#" alt="" class="img-fluid"> Logo
+                        </a>
+                    </div>
+
+                    <div class="d-flex align-items-center">
+                        <a href="#" class="">
+                            <img src="#" alt="" class="img-fluid"> Sprog
+                        </a>
+                        <a href="#" class="ms-4">
+                            <img src="#" alt="" class="img-fluid"> Kurv
+                        </a>
+                    </div>
+                </div>
             </div>
+
+            <div class="col-1"></div>
         </div>
     </div>
-</div>
+</nav
 
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
