1ec1e35eefe5ad3e4b17bf9205056094c95fdf66
emmao <emf002@edu.zealand.dk>
2024-05-23 13:54:10 +0200

    Slettet "root" i $DB_PASS

--- settings/init.php
+++ settings/init.php
@@ -11,7 +11,7 @@ if(CONFIG_LIVE == 0){
     $DB_SERVER = "localhost";
     $DB_NAME = "roomservice";
     $DB_USER = "root";
-    $DB_PASS = "root";
+    $DB_PASS = "";
 }else{
     $DB_SERVER = "";
     $DB_NAME = "";
