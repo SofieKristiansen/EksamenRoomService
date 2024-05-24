c2ea9b3e32a8a555126dee21972fec7113e47217
emmao <emf002@edu.zealand.dk>
2024-05-24 18:04:46 +0200

    Tilsat ingredienser

--- produkt.php
+++ produkt.php
@@ -27,31 +27,80 @@ include("navbar.php");
 include("tilbagepil.php");
 ?>
 
-<div class="container-fluid mt-5 pt-5">
+
+<div class="container-fluid mt-5 ">
     <div class="row">
         <div class="col-1"></div>
 
-        <div class="col-10 overskrift text-primærtekstfarve">
+        <div class="col-10 text-primærtekstfarve">
 
             <?php
-            $sqlprodukter = "SELECT * FROM produkter";
-            $produkter = $db->sql($sqlprodukter);
+
+            $sql = "SELECT * FROM produkter INNER JOIN kategorier ON prodKategoriId = kateId ORDER BY prodNavn ASC LIMIT 1";
+            $produkter = $db->sql($sql, $bind);
             foreach($produkter as $produkt) {
+
                 ?>
-
-                <div class="col-6">
-                    <a href="produkter.php?kategoriId=<?php echo $produkt->prodId?>">
-                        <h2 class="display-3 fw-medium"><?php echo $produkt->prodNavn?></h2>
-                    </a>
-
+                <div class="kategori mb-5 d-flex">
+                    <div class="row ">
+                        <div class="col-10">
+                            <h2 class="display-1 overskrift fw-medium"><?php echo $produkt->prodNavn?></h2>
+                        </div>
+                    </div>
                 </div>
-
                 <?php
             }
             ?>
+
         </div>
         <div class="col-1"></div>
     </div>
+</div>
+
+
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-1"></div>
+
+        <div class="col-10">
+                <div class="col-6 ">
+                        <div class="card shadow bg-kortfarve pt-4 pb-3" style="border-radius: 70px;">
+
+
+
+                            <div class="card-body">
+                                <div class="brødtekst text-primærtekstfarve fs-1 fw-bold">Ingredientliste</div>
+
+                                <?php
+
+                                $sql = "SELECT * FROM produkter INNER JOIN ingredienser ON ingrProdukterId = prodId ORDER BY ingrNavn ASC";
+                                $produkter = $db->sql($sql, $bind);
+                                foreach($produkter as $produkt) {
+
+                                ?>
+
+                                <ul class="brødtekst text-primærtekstfarve fs-2 pt-4">
+                                   <li><?php echo $produkt-> ingrNavn?></li>
+                                </ul>
+
+
+                            <?php
+                            }
+                            ?>
+                            <div class="d-flex justify-content-end pe-3">
+                                <a href="forside.php">
+                                    <button type="button" class="btn shadow me-3 rounded-pill btn-primærknap fs-2 brødtekst" style="width: 150px;">Tilpas</button>
+                                </a>
+                            </div>
+                        </div>
+
+                        </div>
+                    </div>
+            </div>
+        </div>
+    <div class="col-1"></div>
 </div>
 
 
