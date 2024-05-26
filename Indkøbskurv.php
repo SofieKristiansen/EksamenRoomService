73a31ed4334d40ba07a3d83a099a0d3bebd6fbf6
sofiekristiansen <sok002@edu.zealand.dk>
2024-05-26 17:38:45 +0200

    tilføjelse af skraldespand og blyant til indkøbskurven

--- Indkøbskurv.php
+++ Indkøbskurv.php
@@ -30,74 +30,69 @@ include("tilbagepil.php");
 <div class="container-fluid">
     <div class="row">
         <div class="col-1"></div>
-
         <div class="col-10">
-
             <div class="mb-5">
                 <h2 class="display-1 overskrift fw-medium">Indkøbskurv</h2>
             </div>
-
             <div class="card shadow bg-kortfarve" style="border-radius: 70px;">
-
                 <div class="row">
                     <?php
                     $sql = "SELECT * FROM produkter INNER JOIN ingredienser ON ingrProdukterId = prodId ORDER BY ingrNavn ASC LIMIT 1";
                     $produkter = $db->sql($sql, $bind);
                     foreach ($produkter as $produkt) {
-                        ?>
-                        <div class="col-3">
-                            <img src="img/<?php echo $produkt->prodProduktBillede ?>" class="img-fluid" alt="" style="border-radius: 70px;">
-                        </div>
-                        <div class="col-9 d-flex flex-column justify-content-between">
-
-                            <div class="col-6 mb-3">
-                                <h2 class="brødtekst text-primærtekstfarve text-bold fs-2 pt-4" style="line-height: 1;"><?php echo $produkt->prodNavn ?></h2>
+                    ?>
+                    <div class="col-3">
+                        <img src="img/<?php echo $produkt->prodProduktBillede ?>" class="img-fluid" alt="" style="border-radius: 70px;">
+                    </div>
+                    <div class="col-9 d-flex flex-column justify-content-between">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <h2 class="brødtekst text-primærtekstfarve text-bold fs-2 pt-4" style="line-height: 1;"><?php echo $produkt->prodNavn ?></h2>
+                            <div>
+                                <img src="img/blyant.webp" class="pt-4" style="width: 35px; height:auto; margin-left: 10px;">
+                                <img src="img/skraldespand.webp" class="pt-4 me-5" style="width: 35px; height:auto; margin-left: 10px;">
                             </div>
-
-                            <div class="brødtekst text-primærtekstfarve fs-1 fw-bold">
-                                <div class="d-flex justify-content-between align-items-end mb-3">
-                                    <div class="dropdown">
-                                        <button class="btn dropdown-toggle bg-white border-outlinefarve fs-1 d-flex justify-content-between brødtekst text-primærtekstfarve ps-4 pe-4 dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border-radius: 70px; width: 100%">
-                                            Antal: 1
-                                        </button>
-                                        <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item" href="#">1</a></li>
-                                            <li><a class="dropdown-item" href="#">2</a></li>
-                                            <li><a class="dropdown-item" href="#">3</a></li>
-                                            <li><a class="dropdown-item" href="#"></a></li>
-                                            <li><a class="dropdown-item" href="#">1</a></li>
-                                            <li><a class="dropdown-item" href="#">1</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="brødtekst text-primærtekstfarve fs-2 fw-bold me-5" id="pris">
-                                        <?php echo number_format($produkt->prodPris, 2); ?> kr.
-                                    </div>
+                        </div>
+                        <div class="brødtekst text-primærtekstfarve fs-1 fw-bold">
+                            <div class="d-flex justify-content-between align-items-end mb-3">
+                                <div class="dropdown">
+                                    <button class="btn dropdown-toggle bg-white border-outlinefarve fs-1 d-flex justify-content-between brødtekst text-primærtekstfarve ps-4 pe-4 dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border-radius: 70px; width: 100%">
+                                        Antal: 1
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">1</a></li>
+                                        <li><a class="dropdown-item" href="#">2</a></li>
+                                        <li><a class="dropdown-item" href="#">3</a></li>
+                                        <li><a class="dropdown-item" href="#">4</a></li>
+                                        <li><a class="dropdown-item" href="#">5</a></li>
+                                        <li><a class="dropdown-item" href="#">6</a></li>
+                                    </ul>
+                                </div>
+                                <div class="brødtekst text-primærtekstfarve fs-2 fw-bold me-5" id="pris">
+                                    <?php echo number_format($produkt->prodPris, 2); ?> kr.
                                 </div>
                             </div>
-                            <?php
-                            }
-                            ?>
-                            </div>
+                        </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
-
             <div class="d-flex justify-content-end align-items-end vstack" style="margin-left: auto">
                 <div class="brødtekst text-primærtekstfarve fs-2 fw-bold me-5 mt-5 mb-3" id="total-pris"> Total:
                     <?php echo number_format($produkt->prodPris, 2); ?> kr.
                 </div>
-
                 <a href="DinBetaling.php">
                     <button type="button" class="btn btn-lg shadow rounded-pill btn-primærknap fs-3 brødtekst ms-3 me-5">
                         Fortsæt
                     </button>
                 </a>
             </div>
-
-
         </div>
         <div class="col-1"></div>
     </div>
 </div>
+
 
         <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
