2174e49ed0801add516627fd81d3868ef223c5f6
sofiekristiansen <sok002@edu.zealand.dk>
2024-05-26 17:02:23 +0200

    Modal til din betaling

--- DinBetaling.php
+++ DinBetaling.php
@@ -96,16 +96,67 @@ include("tilbagepil.php");
                                     </label>
                                 </div>
                                 <a href="#">
-                                    <button type="button" class="btn btn-lg shadow rounded-pill btn-primærknap fs-3 brødtekst me-5">
+                                    <button type="button" class="btn btn-lg shadow rounded-pill btn-primærknap fs-3 brødtekst me-5" data-bs-toggle="modal" data-bs-target="#modal">
                                         Betal
                                     </button>
                                 </a>
+
                             </div>
                             <div class="form-check p-3">
                                 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault3">
                                 <label class="form-check-label" for="flexCheckDefault3">
                                     Betal ved levering
                                 </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content border-outlinefarve">
+
+                                <div class="modal-header">
+                                    <div class="modal-title text-primærtekstfarve" id="exampleModalLabel"></div>
+                                    <button type="button" class="btn-close btn-close-primærfarve lukkeknap" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+
+                                <div class="modal-body">
+                                    <p class="fs-2 text-primærtekstfarve brødtekst">Betalingsmetoder</p>
+
+                                    <div class="btn-group-vertical" role="group" aria-label="Vertical radio toggle button group">
+                                        <input type="radio" class="btn-check" name="vbtn-radio" id="vbtn-radio1" autocomplete="off">
+                                        <label class="btn btn-outline-primærfarve d-flex align-items-center" style="width: 300px;" for="vbtn-radio1">
+                                            <img src="img/visa.png" style="width: 45px; height:auto;">
+                                            <span class="bg-white border-outlinefarve fs-4 brødtekst text-primærtekstfarve ms-5">Visa</span>
+                                        </label>
+                                        <input type="radio" class="btn-check" name="vbtn-radio" id="vbtn-radio2" autocomplete="off">
+                                        <label class="btn btn-outline-primærfarve d-flex align-items-center" style="width: 300px;" for="vbtn-radio2">
+                                            <img src="img/dankort.png" style="width: 50px; height:auto;">
+                                            <span class="bg-white border-outlinefarve fs-4 brødtekst text-primærtekstfarve ms-5">Dankort</span>
+                                        </label>
+                                        <input type="radio" class="btn-check" name="vbtn-radio" id="vbtn-radio3" autocomplete="off">
+                                        <label class="btn btn-outline-primærfarve d-flex align-items-center" style="width: 300px;" for="vbtn-radio3">
+                                            <img src="img/paypal.png" style="width: 50px; height:auto;">
+                                            <span class="bg-white border-outlinefarve fs-4 brødtekst text-primærtekstfarve ms-5">PayPal</span>
+                                        </label>
+                                        <input type="radio" class="btn-check" name="vbtn-radio" id="vbtn-radio4" autocomplete="off">
+                                        <label class="btn btn-outline-primærfarve d-flex align-items-center" style="width: 300px;" for="vbtn-radio4">
+                                            <img src="img/mobilepay.png" style="width: 50px; height:auto;">
+                                            <span class="bg-white border-outlinefarve fs-4 brødtekst text-primærtekstfarve ms-5">MobilePay</span>
+                                        </label>
+                                        <input type="radio" class="btn-check" name="vbtn-radio" id="vbtn-radio5" autocomplete="off">
+                                        <label class="btn btn-outline-primærfarve d-flex align-items-center" style="width: 300px;" for="vbtn-radio5">
+                                            <img src="img/mastercard.png" style="width: 50px; height:auto;">
+                                            <span class="bg-white border-outlinefarve fs-4 brødtekst text-primærtekstfarve ms-5">Mastercard</span>
+                                        </label>
+                                    </div>
+
+                                    <p class="fs-4 mt-3 text-primærtekstfarve brødtekst">Du bliver omdirigeret til din betalingsmetode via sikre (SSL) sider. Her vil du kunne fuldføre din kreditkortbetaling for din bestilling.</p>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <a href="#" class="btn btn-primary me-3 btn-lg rounded-pill btn-primærknap text-sekundærekstfarve border-outlinefarve" style="width: 150px;">Fortsæt</a>
+                                </div>
                             </div>
                         </div>
                     </div>
