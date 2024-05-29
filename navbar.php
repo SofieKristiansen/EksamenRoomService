42f7439528127ed6a180263d36d59d0ad6b3bdcb
emmao <emf002@edu.zealand.dk>
2024-05-29 09:29:39 +0200

    .

--- navbar.php
+++ navbar.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $cartCount = !empty($_SESSION['cart']) ? array_sum($_SESSION['cart']) : 0;
+
 ?>
 
 <nav class="navbar bg-sekundærfarve" style="height: 110px">
@@ -18,12 +19,13 @@ $cartCount = !empty($_SESSION['cart']) ? array_sum($_SESSION['cart']) : 0;
                         </a>
                         <a href="Indkøbskurv.php" class="ms-4 position-relative">
                             <img src="img/Indkøbskurv.webp" alt="Indkøbskurv" class="img-fluid col-1" style="width: 60px">
-                            <span id="cart-badge" class="fs-3 position-absolute top-25 start-100 translate-middle badge bg-baggrundsfarve rounded-circle">
-                                <?php if ($cartCount > 0): ?>
+                            <span id="cart-badge" class=" fs-3 position-absolute top-25 start-100 translate-middle badge bg-baggrundsfarve rounded-circle">
+                                <?php
+                                if ($cartCount > 0){ ?>
                                     <?php echo $cartCount; ?>
-                                <?php else: ?>
+                                <?php } else { ?>
                                     <span class="visually-hidden"></span>
-                                <?php endif; ?>
+                                <?php } ?>
                             </span>
                         </a>
                     </div>
@@ -34,13 +36,16 @@ $cartCount = !empty($_SESSION['cart']) ? array_sum($_SESSION['cart']) : 0;
     </div>
 </nav>
 
+
 <div class="modal fade" id="sprogModal" tabindex="-1" aria-labelledby="sprogModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content border-outlinefarve">
+        <div class="modal-content border-outlinefarve ">
+
             <div class="modal-header">
                 <h5 class="modal-title" id="sprogModalLabel"></h5>
                 <button type="button" class="btn-close btn-close-primærfarve lukkeknap" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+
             <div class="modal-body text-primærtekstfarve brødtekst">
                 <p class="fs-2 fw-bold mb-4">Hvilket sprog foretrækker du?</p>
                 <div class="form-check fs-3 mb-4">
@@ -56,12 +61,16 @@ $cartCount = !empty($_SESSION['cart']) ? array_sum($_SESSION['cart']) : 0;
                     <label class="form-check-label ms-2" for="deutsch">Deutsch</label>
                 </div>
             </div>
+
             <div class="modal-footer">
                 <button type="button" class="btn me-3 btn-lg rounded-pill btn-sekundærknap brødtekst text-primærtekstfarve border-outlinefarve" data-bs-dismiss="modal" style="width: 160px;">Annuller</button>
                 <button type="button" class="btn btn-lg rounded-pill btn-primærknap brødtekst" data-bs-dismiss="modal" style="width: 160px;">Gem</button>
             </div>
+
         </div>
     </div>
 </div>
 
+
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
