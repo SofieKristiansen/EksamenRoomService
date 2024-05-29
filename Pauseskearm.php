6443a08c36a8e94e4599895f1db55921352370fd
sarahnymann <saj004@edu.zealand.dk>
2024-05-29 14:48:53 +0200

    Tilføjet video og lavet et nu side kaldet Pauseskearm.php

---
+++ Pauseskearm.php
@@ -0,0 +1,28 @@
+<?php
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Produkt</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-baggrundsfarve">
+
+<video class="pausekearm" autoplay loop muted>
+    <source src="pauseVid/PauseVideo.mp4" type="video/mp4">
+    Din browser understøtter ikke HTML5 video.
+</video>
+
+</body>
+</html>
