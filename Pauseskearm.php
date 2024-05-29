d0423f809b5e9177044921c05d442469a072db2c
sarahnymann <saj004@edu.zealand.dk>
2024-05-29 15:48:08 +0200

    Pauseskærm oprettet og styling i CSS

--- Pauseskearm.php
+++ Pauseskearm.php
@@ -1,28 +1,25 @@
-<?php
-require "settings/init.php";
-?>
 <!DOCTYPE html>
 <html lang="da">
 <head>
     <meta charset="utf-8">
-
     <title>Produkt</title>
-
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
-
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-
-<body class="bg-baggrundsfarve">
+<body>
 
 <video class="pausekearm" autoplay loop muted>
     <source src="pauseVid/PauseVideo.mp4" type="video/mp4">
-    Din browser understøtter ikke HTML5 video.
 </video>
+
+<div class="overlay-content">
+    <a href="forside.php" class="btn me-3 btn-lg rounded-pill btn-primærknap brødtekst d-flex justify-content-center align-items-center" style="width: 520px; height: 100px;">
+        <h1 class="m-0">Bestil Roomservice</h1>
+    </a>
+</div>
 
 </body>
 </html>
